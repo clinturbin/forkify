f5fc6f7d036ccec176be0abbeb9b1859e80fbe49
clinturbin <clinturbin@gmail.com>
2019-07-05 09:57:25 -0400

    Started building the search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,34 @@
-// https://www.food2fork.com/api/search
-// https://www.food2fork.com/api/get 
-
-
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+/* 
+    Global State of the App
+    - Search object
+    - Current recipe object
+    - Shopping List object
+    - Liked recipes
+*/
+
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get the query from the view
+    const query = 'pizza'; //ToDo
+
+    if(query) {
+        // 2) New search object and add it to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        console.log(state.search.result);
+    }
+};
+
+document.querySelector('.searhc').addEventListener('submit', event => {
+    event.preventDefault();
+    controlSearch();
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,11 +7,10 @@ export default class Search {
 
     async getResults() {
         const proxy = 'https://cors-anywhere.herokuapp.com/';
-        const key = 'API_KEY_GOES_HERE';
+        const key = 'KEY_GOES_HERE';
         try {
             const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&${this.query}`);
             this.result = res.data.recipes;
-            // console.log(this.result);
         } catch (error) {
             console.log(error);
         }
