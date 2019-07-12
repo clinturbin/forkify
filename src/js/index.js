d630d5d3e60faf2c5e5bd3c1cf278e91c51fffc4
clinturbin <clinturbin@gmail.com>
2019-07-12 16:27:10 -0400

    Added parseIngredients method ro Recipe class to convert each ingredient string with count unit and ingredient

--- src/js/index.js
+++ src/js/index.js
@@ -76,8 +76,9 @@ const controlRecipe = async () => {
         state.recipe = new Recipe(id);
 
         try {
-            // Get recipe data
+            // Get recipe data and parse ingredients
             await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
 
             // Caluclate servings and time
             state.recipe.calcTime();
